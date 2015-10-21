b4f04812cd01e8e7c115ef5b28b97b0df560d6ac
tuouo <ltytuotuo@gmail.com>
2015-10-21 17:14:31 +0800

    modify of shift and

---
+++ shift_or.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+def pre(tarStr):
+    p = {}
+    shirt = 1
+    for i in range(len(tarStr)):
+        if tarStr[i] in p:
+            p[tarStr[i]] ^= shirt
+        else:
+            p[tarStr[i]] = ~shirt
+        shirt <<= 1
+    return p
+
+def shift_or(findStr, tarStr, start = 0):
+    mask = 1 << (len(tarStr) - 1)
+    p = pre(tarStr)
+    d = 0
+    for i in range(start, len(findStr)):
+        d = (d << 1) | p[findStr[i]]
+        if not (d & mask): return i + 1 - len(tarStr) - start
+
+import unittest
+class soTest(unittest.TestCase):
+    def test_so(self):
+        self.assertEqual(shift_or('aababacb', 'ababac', start = 1), 0)
+    def test_so2(self):
+        self.assertEqual(shift_or('aababacb', 'acb'), 5)
+
+if __name__ == '__main__':
+    unittest.main()
