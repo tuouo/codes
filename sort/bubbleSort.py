82930f21cde0b2e2b7112c80ffd158f4a3180da8
tuouo <ltytuotuo@gmail.com>
2015-10-26 00:50:13 +0800

    small&big move, less check

--- sort/bubbleSort.py
+++ sort/bubbleSort.py
@@ -3,10 +3,26 @@
 
 def bubbleSort(arr):
     aLen = len(arr)
-    for i in range(aLen):
-        for j in range(aLen - 1, i, -1):
+    ok = 0
+    while ok < aLen:
+        start = ok
+        ok = aLen
+        for j in range(aLen - 1, start, -1):
             if arr[j - 1] > arr[j]:
-                arr[j - 1], arr[j] = arr[j], arr[j - 1]            
+                arr[j - 1], arr[j] = arr[j], arr[j - 1]
+                ok = j                
+    return arr
+
+def bubbleSort2(arr):
+    aLen = len(arr)
+    ok = aLen
+    while ok > 0:
+        start = ok
+        ok = 0
+        for j in range(1, start):
+            if arr[j - 1] > arr[j]:
+                arr[j - 1], arr[j] = arr[j], arr[j - 1]
+                ok = j                
     return arr
 
 import unittest
@@ -15,8 +31,10 @@ class tBubbleSort(unittest.TestCase):
         self.arr = [3,5,9,8,4,2,1,0,-6,12,-8]
         self.result = [-8,-6,0,1,2,3,4,5,8,9,12]
 
-    def test_bubble(self):
+    def test_bubble1(self):
         self.assertEqual(bubbleSort(self.arr), self.result)
+    def test_bubble2(self):
+        self.assertEqual(bubbleSort2(self.arr), self.result)
 
 if __name__ == '__main__':
     unittest.main()
