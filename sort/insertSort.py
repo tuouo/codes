4b00199ea74e7801edf2d9af0f5acdff1febd7e4
tuouo <ltytuotuo@gmail.com>
2015-10-25 09:39:48 +0800

    setUp

--- sort/insertSort.py
+++ sort/insertSort.py
@@ -13,10 +13,12 @@ def insertSort(arr):
 
 import unittest
 class tInsertSort(unittest.TestCase):
+    def setUp(self):
+        self.arr = [3,5,9,8,4,2,1,0,-6,12,-8]
+        self.result = [-8,-6,0,1,2,3,4,5,8,9,12]
+
     def test_is(self):
-        arr = [3,5,9,8,4,2,1,0,-6,12,-8]
-        result = [-8,-6,0,1,2,3,4,5,8,9,12]
-        self.assertEqual(insertSort(arr), result)
+        self.assertEqual(insertSort(self.arr), self.result)
 
 if __name__ == '__main__':
     unittest.main()
