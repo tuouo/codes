fd166950eb972bd9a9d17ef3bac0934e78d62d31
tuouo <ltytuotuo@gmail.com>
2015-10-31 22:28:50 +0800

    2 ^ 63 -1

---
+++ Bitset.py
@@ -0,0 +1,9 @@
+#！/usr/bin/env/python3
+# -*- coding: utf-8 -*-
+"simulation of Java Bitset"
+# https://www.python.org/dev/peps/pep-0237/
+# import sys
+# print(sys.maxsize)
+
+class BitSet(object):
+    
