b540b691f099628c1fa24c8d6e36836da0cc5c74
yang luo <ltytuotuo@gmail.com>
2017-03-21 23:37:26 +0800

    optimize(when short, suitable sentry, same value)

--- sort/quickSort.py
+++ sort/quickSort.py
@@ -1,37 +1,39 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import unittest
 
-def quickSort(arr):
-    #return quickSort1(arr, 0, len(arr) - 1)
-    return quickSort0(arr, 0, len(arr) - 1)
 
-def quickSort0(arr, l, r):
-    sLen = r - l
-    if sLen < 0:
-        return
-    if sLen < 4:				# not real len 
-        for low in range(1, sLen):
-            for high in range(low + l, l, -1):
+def sort(arr):
+    # return quickSort1(arr, 0, len(arr) - 1)
+    return sort_optimize(arr, 0, len(arr) - 1)
+
+
+def sort_optimize(arr, start, end):
+    len_a = end - start
+    if len_a < 2:
+        return arr
+    elif len_a < 4:  # deal with short arr
+        for pos in range(1, len_a + 1):
+            for high in range(start + pos, start, -1):
                 if arr[high - 1] > arr[high]:
                     arr[high - 1], arr[high] = arr[high], arr[high - 1]
                 else:
                     break
         return arr
 
-    mid = l + ((r - l) >> 1)
-    if arr[mid] > arr[r]:
-        arr[mid], arr[r] = arr[r], arr[mid]
-    if arr[l] > arr[r]:
-        arr[l], arr[r] = arr[r], arr[l]
-    if arr[mid] > arr[l]:
-        arr[l], arr[mid] = arr[mid], arr[l]
-    x = arr[l]
+    mid = start + ((end - start) >> 1)  # check a better sentry
+    if arr[mid] > arr[end]:
+        arr[mid], arr[end] = arr[end], arr[mid]
+    if arr[start] > arr[end]:
+        arr[start], arr[end] = arr[end], arr[start]
+    if arr[mid] > arr[start]:
+        arr[start], arr[mid] = arr[mid], arr[start]
+    x = arr[start]
 
-    low, high = l, r
-    left, right = l, r	#value's(same  of x) position end
+    low, left, right, high = start, start, end, end
     while low < high:
         while low < high and arr[high] >= x:
-            if arr[high] == x:
+            if arr[high] == x:  # for same item
                 arr[right], arr[high] = arr[high], arr[right]
                 right -= 1
             high -= 1
@@ -39,7 +41,7 @@ def quickSort0(arr, l, r):
             arr[low] = arr[high]
             low += 1
         while low < high and arr[low] <= x:
-            if arr[low] == x:
+            if arr[low] == x:  # for same item
                 arr[left], arr[high] = arr[high], arr[left]
                 left += 1
             low += 1
@@ -48,28 +50,27 @@ def quickSort0(arr, l, r):
             high -= 1
     arr[low] = x
 
-    leftLen = left - l
-    rightLen = r - right
-    i, j = left, low - 1
-    while i > l:
-        arr[i], arr[j] = arr[j], arr[i]
-        i -= 1
-        j -= 1
-    i, j = low + 1, right
-    while j < r:
-        arr[i], arr[j] = arr[j], arr[i]
-        i += 1
-        j += 1 
+    len_left_same, len_right_same = left - start, end - right
 
-    quickSort1(arr, l, low - 1 - leftLen)
-    quickSort1(arr, low + 1 + rightLen, r)
+    i, j = left, low - 1
+    while i > start:  # move same item
+        arr[i], arr[j] = arr[j], arr[i]
+        i, j = i - 1, j - 1
+    i, j = low + 1, right
+    while j < end:
+        arr[i], arr[j] = arr[j], arr[i]
+        ii, j = i + 1, j + 1
+
+    sort_optimize(arr, start, low - 1 - len_left_same)
+    sort_optimize(arr, low + 1 + len_right_same, end)
     return arr
 
-def quickSort1(arr, l, r):
-    if l > r:
+
+def sort_origin(arr, start, end):
+    if start > end:
         return
-    x = arr[l]
-    low, high = l, r
+    x = arr[start]
+    low, high = start, end
     while low < high:
         while low < high and arr[high] >= x:
             high -= 1
@@ -82,30 +83,31 @@ def quickSort1(arr, l, r):
             arr[high] = arr[low]
             high -= 1
     arr[low] = x
-    quickSort1(arr, l, low - 1)
-    quickSort1(arr, low + 1, r)
+    sort_origin(arr, start, low - 1)
+    sort_origin(arr, low + 1, end)
     return arr
 
 
-# life is short, I wrote Python
-def quickSort2(arr):
-    if arr == []:
-        return []
+def sort_recursion(arr):
+    if not arr:
+        return arr
     x = arr[0]
-    small = quickSort2([low for low in arr[1:] if low < x])
-    big = quickSort2([low for low in arr[1:] if low >= x])
+    small = sort_recursion([low for low in arr[1:] if low < x])
+    big = sort_recursion([low for low in arr[1:] if low >= x])
     return small + [x] + big
 
-import unittest
-class tQuickSort(unittest.TestCase):
+
+class TestSort(unittest.TestCase):
     def setUp(self):
-        self.arr = [3,5,9,8,4,2,1,0,-6,12,-8]
-        self.result = [-8,-6,0,1,2,3,4,5,8,9,12]
+        self.arr = [3, 5, 9, 8, 4, 2, 1, 0, -6, 12, -8]
+        self.result = [-8, -6, 0, 1, 2, 3, 4, 5, 8, 9, 12]
 
     def test_quick(self):
-        self.assertEqual(quickSort(self.arr), self.result)
+        self.assertEqual(sort(self.arr), self.result)
+
     def test_quick2(self):
-        self.assertEqual(quickSort2(self.arr), self.result)
+        self.assertEqual(sort_recursion(self.arr), self.result)
+
 
 if __name__ == '__main__':
     unittest.main()
