62d6b5a2b6c1d13596ed03d8c5a7768a5cced222
yang luo <ltytuotuo@gmail.com>
2017-03-21 23:33:27 +0800

    reformat & check

--- sort/mergeSort.py
+++ sort/mergeSort.py
@@ -1,26 +1,29 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import unittest
 
-def mergeSort(arr):
-    aLen = len(arr)
-    if aLen <= 4:
-        for i in range(1, aLen):
+
+def sort(arr):
+    len_a = len(arr)
+    if len_a < 2:
+        return arr
+    elif len_a <= 4:
+        for i in range(1, len_a):
             for j in range(i, 0, -1):
                 if arr[j - 1] > arr[j]:
                     arr[j - 1], arr[j] = arr[j], arr[j - 1]
                 else:
                     break
         return arr
-    mid = aLen // 2
-    left = mergeSort(arr[:mid])
-    right = mergeSort(arr[mid:])
+    mid = len_a // 2
+    left = sort(arr[:mid])
+    right = sort(arr[mid:])
     return merge(left, right)
 
+
 def merge(left, right):
-    result = []
-    i, j = 0, 0
-    llen, rLen = len(left), len(right)
-    while i < llen and j < rLen:
+    result, i, j = [], 0, 0
+    while i < len(left) and j < len(right):
         if left[i] <= right[j]:
             result.append(left[i])
             i += 1
@@ -31,14 +34,15 @@ def merge(left, right):
     result += right[j:]
     return result
 
-import unittest
-class tMergeSort(unittest.TestCase):
+
+class TestSort(unittest.TestCase):
     def setUp(self):
-        self.arr = [3,5,9,8,4,2,1,0,-6,12,-8]
-        self.result = [-8,-6,0,1,2,3,4,5,8,9,12]
+        self.arr = [3, 5, 9, 8, 4, 2, 1, 0, -6, 12, -8]
+        self.result = [-8, -6, 0, 1, 2, 3, 4, 5, 8, 9, 12]
 
     def test_merge(self):
-        self.assertEqual(mergeSort(self.arr), self.result)
+        self.assertEqual(sort(self.arr), self.result)
+
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
