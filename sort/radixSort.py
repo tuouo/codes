9e4e9cd031df2cf12a4898b4a0141c6dfff5a7ee
yang luo <ltytuotuo@gmail.com>
2017-03-21 23:34:07 +0800

    all item should >0 & radix: <=10

--- sort/radixSort.py
+++ sort/radixSort.py
@@ -1,8 +1,35 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # 2017/3/21
+import math
+import unittest
 
+
+def sort(arr, radix=10):
+    """
+    :param arr: all item should >0
+    :param radix: <=10
+    :return:
+    """
+    digit = int(math.ceil(math.log(max(arr), radix)))
+    for i in range(1, digit+1):
+        bucket = [[] for _ in range(radix)]
+        for item in arr:
+            bucket[item % (radix**i) // (radix**(i-1))].append(item)
+        del arr[:]
+        for each in bucket:
+            arr.extend(each)
+    return arr
+
+
+class TestSort(unittest.TestCase):
+    def setUp(self):
+        self.arr = [3, 5, 9, 8, 4, 2, 1, 0, 12]
+        self.result = [0, 1, 2, 3, 4, 5, 8, 9, 12]
+
+    def test_sort(self):
+        self.assertEqual(sort(self.arr), self.result)
 
 
 if __name__ == '__main__':
-    pass
+    unittest.main()
