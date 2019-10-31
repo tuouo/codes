ae625ad09a9464ec1b44e25ac70ded7921986ac3
young luo <luotengyang@outlook.com>
2019-10-31 23:50:06 +0800

    wisdom of quine

---
+++ wisdom/quine/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author: young
+# date: 2019/10/31
+
+
+if __name__ == '__main__':
+    pass
---
+++ wisdom/quine/quine3.py
@@ -0,0 +1,1 @@
+print((lambda s: s%s)('print((lambda s: s%%s)(%r))'))
