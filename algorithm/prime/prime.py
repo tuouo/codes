28cc123e039410f716dae93fce2b24bfbc29b063
young luo <ltytuotuo@gmail.com>
2019-11-22 16:15:12 +0800

    remove to file algorithm

--- algorithm/prime/prime.py
+++ algorithm/prime/prime.py
@@ -4,7 +4,21 @@
 import unittest
 
 
-def primes(n):
+def primes(n=10000):  # 1229
+    arr = [True] * n
+    arr[0] = arr[1] = False
+    arr[4:n:2] = [False] * ((n - 1) // 2 - 1)
+    for i in range(3, int((n - 1) ** 0.5) + 1):
+        if arr[i]:
+            arr[i * i:n:2 * i] = [False] * ((n - 1 - i * i) // (2 * i) + 1)
+    # arr = [i for i, p in enumerate(arr) if p]
+    return arr
+
+
+sieve_of_eratosthenes = primes
+
+
+def primes_directly(n):
     arr = [True] * n
     arr[0] = arr[1] = False
     for i in range(2, int((n - 1) ** 0.5) + 1):
@@ -30,8 +44,10 @@ class TestCase(unittest.TestCase):
         del self.primes
 
     def test_prime(self):
-        self.assertEqual(self.primes, [i for i, j in enumerate(primes(100)[0]) if j])
-        self.assertEqual(len(self.primes), primes(100)[1])
+        self.assertEqual(self.primes, [i for i, j in enumerate(primes_directly(100)[0]) if j])
+        self.assertEqual(len(self.primes), primes_directly(100)[1])
+
+        self.assertEqual(self.primes, [i for i, j in enumerate(primes(100)) if j])
 
 
 if __name__ == '__main__':
